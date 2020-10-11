a981fc863722f4b86e20ee5d5a23eccb14d36649
nurulaziza <mnurulaziza@gmail.com>
2020-10-11 18:34:23 +0700

    add biodata function

--- application/controllers/Landing.php
+++ application/controllers/Landing.php
@@ -3,21 +3,55 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Landing extends CI_Controller {
 
-	public function __construct() {
+  public function __construct() 
+  {
 		parent::__construct();
 		$this->load->helper(array('url','form'));
 		$this->load->model('landing_m');
 		$this->load->library('session');
   }
   
-  function index(){
-    $this->load->view('layout/landing/header');
+  function index()
+  {
+    $head['title'] = 'Landing';
+
+    $this->load->view('layout/landing/header', $head);
     $this->load->view('layout/landing/nav_header');
     $this->load->view('landing/index');
     $this->load->view('layout/landing/footer');
   }
 
-  function test(){
+  function cek_user_email()
+  {
+
+  }
+
+  function biodata()
+  {
+    $head['title'] = 'Biodata';
+
+    // $data;
+
+    $this->load->view('layout/landing/header', $head);
+    $this->load->view('layout/landing/nav_header_logo');
+    $this->load->view('landing/index');
+    $this->load->view('layout/landing/footer');
+  }
+
+  function screening()
+  {
+    $head['title'] = 'Scrining';
+
+    // $data;
+
+    $this->load->view('layout/landing/header', $head);
+    $this->load->view('layout/landing/nav_header_logo');
+    $this->load->view('landing/biodata');
+    $this->load->view('layout/landing/footer');
+  }
+
+  function test()
+  {
     $this->load->view('publik/index');
   }
 }
