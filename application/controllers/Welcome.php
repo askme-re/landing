ac9ca6d7a186beff4b3f7eef211ce64b4b30a339
lilisya <lilis.bigio.id>
2020-10-17 00:27:36 +0700

    deploye kit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -143,6 +143,7 @@ class Welcome extends CI_Controller {
 				// Write question
 				$result .= '<div class="col-sm-10 ">
 							  <div class="form-group">
+							  <label class="form-check-label">'.$nQuiz.'</label>	
 								<label class="form-check-label">'.$v->pertanyaan.'</label>';
 				
 				// Set id of question
