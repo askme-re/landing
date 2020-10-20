4fc1ecbab28715b8839d2caaca5a9a07955f755c
lilisya <lilis.bigio.id>
2020-10-20 22:32:18 +0700

    setup ssl and route

--- application/views/landing/index.php
+++ application/views/landing/index.php
@@ -256,7 +256,7 @@ penyaringan 16 wabah kasus menular COVID-19 dilakukan skrining dengan beberapa p
 								<input type="hidden" name="<?= $this->security->get_csrf_token_name(); ?>" value="<?= $this->security->get_csrf_hash(); ?>" style="display: none">
 								
 								<label class="col-form-label">
-									Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus.<br/>Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum. 
+									Anda akan diminta mengisi data pribadi untuk kepentingan skrining, dengan mengisi data dibawah Anda menyatakan setuju dengan syarat dan kondisi.
 								</label>
 							</div>
 							
