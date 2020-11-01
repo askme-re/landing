89fa5f20ae2d5b28995b123cd9fcbfe1cc0f2e92
layumi3 <yumaim.lacorp@gmail.com>
2020-11-01 12:06:53 +0700

    revisi per 30 typo

--- application/models/Landing_model.php
+++ application/models/Landing_model.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 if(!defined('BASEPATH')) exit('no direct script access allowed');
 
 class Landing_model extends CI_Model
@@ -13,7 +13,7 @@ class Landing_model extends CI_Model
      if (isset($where) or $where != NULL) {
 				$this->db->where($where);
 		}
-		
+
 		$query = $this->db->get('user');
 		if ($query->num_rows() > 0) {
 				if (isset($where) or $where != NULL) {
@@ -24,13 +24,13 @@ class Landing_model extends CI_Model
 		}
 		return FALSE;
   }
-	
+
   function get_jenis_skrining($where=NULL)
   {
     if (isset($where) or $where != NULL) {
 				$this->db->where($where);
 		}
-		
+
 		$query = $this->db->get('jns_wabah');
 		if ($query->num_rows() > 0) {
 				if (isset($where) or $where != NULL) {
@@ -41,35 +41,35 @@ class Landing_model extends CI_Model
 		}
 		return FALSE;
 	}
-	
+
 	function save_user($data){
 		$this->db->insert('user', $data);
 		$insert_id = $this->db->insert_id();
 
 		return  $insert_id;
 	}
-	
+
 	function quizes($jenis){
 		$str = "SELECT f.id, f.pertanyaan,f.jenis,b.id AS id_bobot,b.bobot,b.jawaban,b.id_pertanyaan
-					,CASE WHEN b.jawaban=1 THEN 'Iya'
+					,CASE WHEN b.jawaban=1 THEN 'Ya'
 						WHEN b.jawaban = 0 THEN 'Tidak' END AS opsi_bobot
-					FROM form f 
+					FROM form f
 					LEFT JOIN bobot b ON b.id_pertanyaan=f.id
 					WHERE f.jenis='".$jenis."' ORDER BY f.id";
 		$qry = $this->db->query($str);
-		
+
 		if ($qry->num_rows() > 0){
 			return $qry->result();
 		}
 		return null;
 	}
-	
+
 	function insert_screening($data){
-		$insert = $this->db->insert_batch('screening', $data); 
-		
+		$insert = $this->db->insert_batch('screening', $data);
+
 		return ($insert) ? true : false ;
 	}
-	
+
 	public function tanya($jenis){
 		$qry = $this->db->query('select * from form where jenis="'.$jenis.'"');
 		if ($qry->num_rows() > 0){
@@ -77,49 +77,49 @@ class Landing_model extends CI_Model
 		}
 		return null;
 	}
-	
-	
+
+
 	function get_provinsi(){
 		$this->db->select('id_prop AS k, nama_prop AS v');
-        $this->db->from('ms_provinsi'); 
+        $this->db->from('ms_provinsi');
         $query = $this->db->get();
-									
+
 		if ($query->num_rows() > 0){
 			return $query->result();
 		}
 		return null;
 	}
-	
+
 	function get_kabupaten($id_prov){
 		$this->db->select('id_kab AS k, nama_kab AS v');
 		$this->db->from('ms_kabupaten');
 		$this->db->where('id_prop', $id_prov);
         $query = $this->db->get();
-									
+
 		if ($query->num_rows() > 0){
 			return $query->result();
 		}
 		return null;
 	}
-	
+
 	function get_kecamatan($id_kab){
 		$this->db->select('id_kec AS k, nama_kec AS v');
 		$this->db->from('ms_kecamatan');
 		$this->db->where('id_kab', $id_kab);
         $query = $this->db->get();
-									
+
 		if ($query->num_rows() > 0){
 			return $query->result();
 		}
 		return null;
 	}
-	
+
 	function get_kelurahan($id_kec){
 		$this->db->select('id_desa AS k, nama_desa AS v');
 		$this->db->from('ms_desa');
 		$this->db->where('id_kec', $id_kec);
         $query = $this->db->get();
-									
+
 		if ($query->num_rows() > 0){
 			return $query->result();
 		}
